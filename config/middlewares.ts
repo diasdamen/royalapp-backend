24ab32ad48c886c68807f37a6ea7f01b7d0919e0
Dias Damen <diasdamen@gmail.com>
2024-07-26 17:30:35 +0500

    upd middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,20 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://royalapp-backend.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://royalapp-backend.onrender.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
